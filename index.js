706fc3350d8b0be1b8d3a803d898a721cba9ff5d
Tilde <tilde@thuryism.net>
2024-10-31 14:24:28 -0700

    add some new dependencies

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const serveStatic = require("serve-static");
 const port = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, "public")));
-
-// Basic route for the home page
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.use(serveStatic(path.join(__dirname, "public")));
 
 // Start the server
 app.listen(port, () => {
